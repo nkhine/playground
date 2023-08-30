e1c51a4e71455e3a18f9de427025620726d982a3
Norman Khine <norman@khine.net>
2023-08-30 11:53:16 +0100

    Exercise 3.3: Color each polygon based on its height

--- go/ch3/main.go
+++ go/ch3/main.go
@@ -18,24 +18,46 @@ var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
 func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"style='stroke: grey; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay, validA := corner(i+1, j)
-			bx, by, validB := corner(i, j)
-			cx, cy, validC := corner(i, j+1)
-			dx, dy, validD := corner(i+1, j+1)
+			ax, ay, z1, validA := corner(i+1, j)
+			bx, by, z2, validB := corner(i, j)
+			cx, cy, z3, validC := corner(i, j+1)
+			dx, dy, z4, validD := corner(i+1, j+1)
 			if validA && validB && validC && validD {
-				fmt.Printf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g'/>\n",
-					ax, ay, bx, by, cx, cy, dx, dy)
+				color := computeColor([]float64{z1, z2, z3, z4})
+				fmt.Printf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g' fill='%s'/>\n",
+					ax, ay, bx, by, cx, cy, dx, dy, color)
 			}
 		}
 	}
 	fmt.Println("</svg>")
 }
 
-func corner(i, j int) (float64, float64, bool) {
+func computeColor(heights []float64) string {
+	avgHeight := (heights[0] + heights[1] + heights[2] + heights[3]) / 4
+
+	// Map the average height to a color.
+	// We'll use a simple linear interpolation between blue and red based on the height.
+	clamp := func(v, min, max float64) float64 {
+		if v < min {
+			return min
+		} else if v > max {
+			return max
+		}
+		return v
+	}
+
+	scale := clamp((avgHeight+0.5)*0.5, 0, 1)
+	r := int(255 * scale)
+	b := int(255 * (1 - scale))
+
+	return fmt.Sprintf("#%02x%02x%02x", r, 0, b)
+}
+
+func corner(i, j int) (float64, float64, float64, bool) {
 	// Find point (x,y) at corner of cell (i,j)
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
@@ -45,13 +67,13 @@ func corner(i, j int) (float64, float64, bool) {
 
 	// Check if z is NaN or Infinity
 	if math.IsNaN(z) || math.IsInf(z, 0) {
-		return 0, 0, false
+		return 0, 0, 0, false
 	}
 
 	// Project (x,y,z) isometrically onto 2-D
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy, true
+	return sx, sy, z, true
 }
 
 func f(x, y float64) float64 {
